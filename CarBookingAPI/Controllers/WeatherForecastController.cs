b7d4c7d16940b8694865b2d3c9e872da3225cdc1
sebastianvlad1 <sebastianbranzas@gmail.com>
2020-04-20 22:13:46 +0300

    Initialize WEB API

--- CarBookingAPI/Controllers/WeatherForecastController.cs
+++ CarBookingAPI/Controllers/WeatherForecastController.cs
@@ -11,29 +11,13 @@ namespace CarBookingAPI.Controllers
     [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+        [HttpGet("actiune/{id?}")]
+        public string Get(int id = 0, [FromQuery] int ziua = 0)
+        {   Console.WriteLine("id = " +  id);
+            Console.WriteLine("ziua = " +  ziua);
+            Console.WriteLine(" =========== ");
+            return "workng...";
         }
     }
 }
